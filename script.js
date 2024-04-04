1c469f75a1a2a1df8f37efb93b904b310d476225
Velination <143023447+Velination@users.noreply.github.com>
2024-04-04 08:59:14 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -28,36 +28,36 @@ blackBackDrop.addEventListener("click", toggleAddTaskForm);
 
 let categories = [
   {
-    title: "personal",
-    Img: "",
+    title: "Personal",
+    img: "boy.png",
   },
   {
     title: "work",
-    img: "",
+    img: "briefcase.png",
   },
   {
     title: "shopping",
-    img: "",
+    img: "shopping.png",
   },
   {
     title: "coding",
-    img: "",
+    img: "web-design.png",
   },
   {
     title: "Healthcare",
-    img: "",
+    img: "healthcare.png",
   },
   {
     title: "Fitness",
-    img: "",
+    img: "dumbbell.png",
   },
   {
     title: "Education",
-    img: "",
+    img: "education.png",
   },
   {
     title: "Finance",
-    img: "",
+    img: "saving.png",
   },
 ];
 
@@ -179,12 +179,27 @@ let tasks = [
   {
     id: 20,
     task: "meditate for 10 minutes",
-    category: "presonal",
+    category: "personal",
     completed: false,
   },
 ];
 
+let selectedCategory = categories[0];
+
 const categoriesContainer = document.querySelector(".categories");
+const categoryTitle = document.querySelector(".category-title");
+const totalCategoryTasks = document.querySelector(".category-tasks");
+const categoryImg = document.querySelector("#category-img");
+const totalTasks = document.querySelector(".totalTasks");
+
+const calculateTotal = () => {
+  const categoryTasks = tasks.filter(
+    (task) =>
+      task.category.toLowerCase() === selectedCategory.title.toLowerCase()
+  );
+  totalCategoryTasks.innerHTML = `${categoryTasks.length} Tasks`;
+  totalTasks.innerHTML = tasks.length;
+};
 
 const renderCategories = () => {
   categoriesContainer.innerHTML = "";
@@ -197,23 +212,34 @@ const renderCategories = () => {
     // create a div to render category
     const div = document.createElement("div");
     div.classList.add("category");
-    div.innerHTML = `  <div class="left">
-    <img src="" alt="">
+    div.addEventListener("click", () => {
+      wrapper.classList.add("show-category");
+      selectedCategory = category;
+      categoryTitle.innerHTML = category.title;
+      categoryImg.src = `images/${category.img}`;
+      calculateTotal();
+
+      // rerender tasks when category change
+      renderTasks();
+    });
+    div.innerHTML = `  
+    <div class="left">
+    <img src="images/${category.img}" alt="${category.title}" />
     <div class="content">
-      <h2>Personal</h2>
-      <p>5 Tasks</p>
+      <h2>${category.title}</h2>
+      <p>${categoryTasks.length}</p>
     </div>
    </div>
    <div class="options">
     <div class="toggle-btn">
       <svg
-xmlns="http://www.w3.org/2000/svg"
-fill="none"
-viewBox="0 0 24 24"
-stroke-width="1.5"
-stroke="currentColor"
-class="w-6 h-6"         
->
+     xmlns="http://www.w3.org/2000/svg"
+     fill="none"
+     viewBox="0 0 24 24"
+      stroke-width="1.5"
+     stroke="currentColor"
+     class="w-6 h-6"         
+       >
 <path
 stroke-linecap="round"
 stroke-linejoin="round"
@@ -221,10 +247,158 @@ d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 
 />
 </svg>
     </div>
-  </div>`;
+    </div>`;
 
     categoriesContainer.appendChild(div);
   });
 };
 
+const tasksContainer = document.querySelector(".tasks");
+
+const renderTasks = () => {
+  tasksContainer.innerHTML = "";
+  const categoryTasks = tasks.filter(
+    (task) =>
+      task.category.toLowerCase() === selectedCategory.title.toLowerCase()
+  );
+
+  // if no task for selected category
+  if (categoryTasks.length === 0) {
+    tasksContainer.innerHTML =
+      '<p class ="no-tasks"> No tasks for this category</p>';
+  } else {
+    // if there are tasks for selected category render them
+    categoryTasks.forEach((task) => {
+      const div = document.createElement("div");
+      div.classList.add("task-wrapper");
+      const label = document.createElement("label");
+      label.classList.add("task");
+      label.setAttribute("for", task.id);
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.id = task.id;
+      checkbox.checked = task.completed;
+
+      // add completion functionality on click checkbox;
+      checkbox.addEventListener("change", () => {
+        const index = tasks.findIndex((t) => t.id === task.id);
+        // change the true to false or vice verse
+        tasks[index].completed = !tasks[index].completed;
+        //  save in local
+        saveLocal();
+      });
+
+      div.innerHTML = `
+      <div class="delete">
+                  <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"         
+                >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.144 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48,667 0 00-7.5 0"          
+                />
+                </svg>
+                </div>
+      `;
+      label.innerHTML = `
+      <span class="checkmark">
+                    <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6">
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M4.5 12.75l6 6 9-13.5"/>
+                </svg>
+                  </span>
+                  <p>${task.task} </p>
+      `;
+
+      label.prepend(checkbox);
+      div.prepend(label);
+      tasksContainer.appendChild(div);
+
+      // delete functionality
+
+      const deleteBtn = div.querySelector(".delete");
+      deleteBtn.addEventListener("click", () => {
+        const index = tasks.findIndex((t) => t.id === task.id);
+
+        // remove the clicked task
+        tasks.splice(index, 1);
+        saveLocal();
+        renderTasks();
+      });
+    });
+    renderCategories();
+    calculateTotal();
+  }
+};
+
+// save and get tasks from local storage
+const saveLocal = () => {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+const getLocal = () => {
+  const localTasks = JSON.parse(localStorage.getItem("tasks"));
+
+  //  if tasks found
+  if (localTasks) {
+    tasks = localTasks;
+  }
+};
+
+// lets add functionality to add new tasks
+
+// render all the categories in select
+const categorySelect = document.querySelector("#category-select");
+const cancelBtn = document.querySelector(".cancel-btn");
+const addBtn = document.querySelector(".add-btn");
+
+const taskInput = document.querySelector("#task-input");
+
+cancelBtn.addEventListener("click", toggleAddTaskForm);
+
+addBtn.addEventListener("click", () => {
+  const task = taskInput.value;
+  const category = categorySelect.value;
+
+  if (task === "") {
+    alert("Please enter a task");
+  } else {
+    const newTask = {
+      id: (tasks.length = 1),
+      task,
+      category,
+      completed: false,
+    };
+    tasks.push(newTask);
+    taskInput.value = "";
+    saveLocal();
+    toggleAddTaskForm();
+    renderTasks();
+  }
+});
+
+categories.forEach((category) => {
+  const option = document.createElement("option");
+  option.value = category.title.toLowerCase();
+  option.textContent = category.title;
+  categorySelect.appendChild(option);
+});
+
+// these are already stored tasks
+getLocal();
+calculateTotal();
 renderCategories();
+renderTasks();
